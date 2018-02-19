04076df280e6fd0472bdb266f0da761761b854b5
Elsa Souyeaux <leia.makkura@gmail.com>
2018-02-19 03:32:28 -0500

    stats : front-end

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -7,11 +7,12 @@ import PropTypes from 'prop-types'
 class Statistics extends React.Component {
     constructor(props) {
         super(props)
-     //   this.getStatsById = this.getStatsById.bind(this)
+        //   this.getStatsById = this.getStatsById.bind(this)
         this.calcProgress = this.calcProgress.bind(this)
         this.state = {
             stats: [],
             goalPercent: 0,
+            goal: 5000,
             sportTime: 0,
             loading: true
         }
@@ -42,22 +43,22 @@ class Statistics extends React.Component {
                 })
         }
     */
-    calcProgress() { 
+    calcProgress() {
         var sportTime = 2500;
         var goal = 5000;
-                /*
+        /*
 
-        this.state.stats.forEach(function (item) {
-            sportTime += item.time;
-        });
-        self.state.stats.forEach(function (item) {
-            progress += item.time;
-        });
-        self.state.progress = (progress * 100) / goal;
-        self.state.sportTime = progress;
-        */
-       this.setState({goalPercent: (sportTime * 100) / goal});
-       this.setState({sportTime: sportTime});
+this.state.stats.forEach(function (item) {
+    sportTime += item.time;
+});
+self.state.stats.forEach(function (item) {
+    progress += item.time;
+});
+self.state.progress = (progress * 100) / goal;
+self.state.sportTime = progress;
+*/
+        this.setState({ goalPercent: (sportTime * 100) / goal });
+        this.setState({ sportTime: sportTime });
     }
 
     render() {
@@ -65,37 +66,94 @@ class Statistics extends React.Component {
         //if (this.state.loading) {
         //    return (false);
         //} else {
-            return (
-                <div id="StatsPage" className="card mb-4 p-sm-3">
-                    <h3>page des statistiques</h3>< br />
+        return (
+            <div id="StatsPage" className="card mb-4 p-sm-3">
+                <h3>Vos statistiques</h3>< br />
 
-                    <div className="statsblock" id="ObjectiveJauge">
-                        barre de progression de l'objectif
-                        <ProgressBar percentage={this.state.progress} backgroundStyle="azure" /><br />
-                    </div>
+                <table className="w-100">
+                    <tbody>
 
-                    <div className="statsblock" id="GamesFav">
-                        jeux favoris
-                       <table className="games-fav">
-                            <tbody>
+                        <tr>
+                            <td className="w-50">
+                                <div className="statsblock center p-4 m-4" id="ObjectiveJauge">
+                                    <div className="display-4 text-info">{this.state.sportTime}</div><br />
+                                    <div className="statdescri">minutes d'effort cette semaine !</div>
+                                </div>
+                            </td>
+                            <td className="w-50">
 
-                                <tr>
-                                    <td><img alt="jeu1" className="GameThumbnail" src="/ressources/jeu1.jpg" /></td>
-                                    <td><img alt="jeu2" className="GameThumbnail" src="/ressources/jeu2.jpg" /></td>
-                                </tr>
-                                <tr>
-                                    <td><img alt="jeu3" className="GameThumbnail" src="/ressources/jeu3.jpg" /></td>
-                                    <td><img alt="jeu4" className="GameThumbnail" src="/ressources/jeu4.jpg" /></td>
-                                </tr>
-                            </tbody>
-                        </table><br />
-                    </div>
+                                <div className="statsblock center p-4 m-4" id="ObjectiveJauge">
+                                    <div className="display-4 text-info">{this.state.goal}</div>
+                                    <span className="statdescri">objectif de la semaine</span>
+                                </div>
+                            </td>
+                        </tr>
 
-                    <div className="statsblock" id="SportTime">
-                        Temps de sport cette semaine : {Math.round(this.state.sportTime / 60)}h
-                    </div>
+                    </tbody>
+                </table>
+                <div className="statsblock p-4 m-4" id="ObjectiveJauge">
+                    <div className="statdescri">Objectif rempli à</div>
+                    <ProgressBar percentage={this.state.goalPercent} backgroundStyle="azure" />
                 </div>
-            )
+
+                <div className="statsblock center p-4 m-4" id="GamesFav">
+                    <span className="statdescri">jeux les plus joués</span>
+                    <table className="w-100 mt-4">
+                        <tbody>
+                            <tr className="w-100">
+
+                                <td className="w-50">
+                                    <div className="card border-info text-center m-3">
+                                        <div className="card-header">
+                                            <img className="statimg card-img-top rounded mx-auto d-block" src="/ressources/jeu1.jpg" alt="Card" />
+                                        </div>
+                                        <div className="card-body">
+                                            <p className="card-text text-info">Jeu1 example</p>
+                                        </div>
+                                    </div>
+                                </td>
+
+                                <td className="w-50">
+                                    <div className="card border-info text-center m-3">
+                                        <div className="card-header">
+                                            <img className="statimg card-img-top rounded mx-auto d-block" src="/ressources/jeu2.jpg" alt="Card" />
+                                        </div>
+                                        <div className="card-body">
+                                            <p className="card-text text-info">Jeu1 example</p>
+                                        </div>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr className="w-100">
+
+                                <td className="w-50">
+                                    <div className="card border-info text-center m-3">
+                                        <div className="card-header">
+                                            <img className="statimg card-img-top rounded mx-auto d-block" src="/ressources/jeu3.jpg" alt="Card" />
+                                        </div>
+                                        <div className="card-body">
+                                            <p className="card-text text-info">Jeu1 example</p>
+                                        </div>
+                                    </div>
+                                </td>
+
+                                <td className="w-50">
+                                    <div className="card border-info text-center m-3">
+                                        <div className="card-header">
+                                            <img className="statimg card-img-top rounded mx-auto d-block" src="/ressources/jeu4.jpg" alt="Card" />
+                                        </div>
+                                        <div className="card-body">
+                                            <p className="card-text text-info">Jeu1 example</p>
+                                        </div>
+                                    </div>
+                                </td>
+
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        )
         //}
     }
 }
@@ -107,10 +165,14 @@ Statistics.contextTypes = {
 
 export default Statistics
 
-    /*
+                /*
+                <div className="statsblock p-4 m-4" id="SportTime">
+                    Temps de sport cette semaine : {Math.round(this.state.sportTime / 60)}h
+                    </div>
+
                         <div class="statsblock" id="Games_fav">
-                        Type d'effort
+                    Type d'effort
             <br />
-                        Graphiques en barre
+                    Graphiques en barre
         </div>
-        */
+                */
