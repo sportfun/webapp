de293394cd6a1fffb6534ff7bd669a1d4a58996a
Elsa Souyeaux <leia.makkura@gmail.com>
2018-11-29 19:03:04 +0100

    Fix-pic

--- src/components/User/Users.js
+++ src/components/User/Users.js
@@ -2,8 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom'
 import ApiManager from '../ApiManager'
 import PropTypes from 'prop-types'
-import profilepic from '../../assets/img/user.png'
-
+import Avatar from '../Avatar'
 
 class Users extends React.Component {
   constructor(props) {
@@ -26,28 +25,28 @@ class Users extends React.Component {
   }
 
   componentWillReceiveProps(nextProps) {
-    this.setState({ users: []})
+    this.setState({ users: [] })
     ApiManager.getUsersByPattern(nextProps.location.state.pattern)
-    .then(users => {
-      this.setState({
-        users: users,
-        pattern: nextProps.location.state.pattern,
+      .then(users => {
+        this.setState({
+          users: users,
+          pattern: nextProps.location.state.pattern,
+        })
       })
-    })
   }
 
 
   render() {
     console.log(this.state.users.length)
-    if(this.state.loading){return null}
+    if (this.state.loading) { return null }
 
     if (this.state.users.length !== 0) {
-    
+
       var listUsers = this.state.users.map((elem, index) => {
         return (
           <Link to={`/profile/${elem.username}`} key={elem._id}>
             <li className="item-user p-sm-3" >
-              <img className="rounded-avatar" alt='profilePicture-item' src={profilepic} />
+              <Avatar isLittle={true} profilepic={elem.profilePic} />
               <div className="info-item-user">
                 {elem.firstName} {elem.lastName}
               </div>
